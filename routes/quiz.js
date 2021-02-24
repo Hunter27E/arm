1b6023be9e7908a513b840ebf46658110481913f
stayingqold <finn@ucsd.edu>
2021-02-24 08:24:12 -0800

    can now dynamically go to new questions

--- routes/quiz.js
+++ routes/quiz.js
@@ -3,14 +3,13 @@
 const quiz = require('../game.json');
 
 exports.view = function (req, res) {
-	// data to pass to template
-	let data = quiz;
-	data.layout = 'quiz';
-	
 	var questionID = req.query;
-	console.log(req.query);
+	question_number = parseInt(questionID["id"]);
 
-	data.question_number = parseInt(questionID["id"]);
+	// data to pass to template
+	let data = quiz["questions"][question_number];
+	
+	data.layout = 'quiz';
 	console.log(data);
 	
 	res.render('quiz', data);
