6f466b09f0c25091495535a66df0e8aaee7bc834
stayingqold <finn@ucsd.edu>
2021-02-23 21:26:16 -0800

    implemented correct/incorrect page; implemented handlebars/json

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,8 +1,17 @@
 // GET ARM quiz page
 
+const quiz = require('../game.json');
+
 exports.view = function (req, res) {
 	// data to pass to template
-	let data = {};
+	let data = quiz;
 	data.layout = 'quiz';
+	
+	var questionID = req.query;
+	console.log(req.query);
+
+	data.question_number = parseInt(questionID["id"]);
+	console.log(data);
+	
 	res.render('quiz', data);
 };
