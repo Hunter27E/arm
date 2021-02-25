bea5815536b9e1f76617f6f3c3a0ad978912b760
Hunter Estrada <hestrada@ucsd.edu>
2021-02-25 00:17:48 -0800

    added back port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const incorrect = require('./routes/incorrect');
 const correct = require('./routes/correct');
 
 // create app and set port
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 // create environment
