943e98fb27e55095f2f8a67e92dc2d0aca3cfaf3
Hunter Estrada <hestrada@ucsd.edu>
2021-02-25 00:06:00 -0800

    added: basic ar, fun facts, ar styling

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const tutorial = require('./routes/tutorial');
 const blank = require('./routes/blank');
 
 // create app and set port
-const port = process.env.PORT || 3000;
+const port = 3000;
 const app = express();
 
 // create environment
