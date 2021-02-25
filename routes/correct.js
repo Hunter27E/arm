f1561f29020a265045cd1f75435f6b598b52ceb1
stayingqold <finn@ucsd.edu>
2021-02-25 08:58:58 -0800

    game now fully works (at least for what we will turn in this week) -- needs styling

--- routes/correct.js
+++ routes/correct.js
@@ -1,8 +1,15 @@
 // GET ARM quiz page
+const quiz = require('../game.json');
 
 exports.view = function (req, res) {
+	var questionID = req.query;
+	question_number = parseInt(questionID["id"]);
+
 	// data to pass to template
-	let data = {};
-	data.layout = 'incorrect';
-	res.render('incorrect', data);
+	let data = quiz["questions"][question_number];
+	
+	data.layout = 'correct';
+	console.log(data);
+	
+	res.render('correct', data);
 };
