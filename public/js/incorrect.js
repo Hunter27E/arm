ffa783d209f92c21d03265ad44d8d7909cc4c9d6
Hunter Estrada <hestrada@ucsd.edu>
2021-02-25 21:09:53 -0800

    hopefully fixed app crashing

--- public/js/incorrect.js
+++ public/js/incorrect.js
@@ -1,11 +1,9 @@
 'use strict';
 
-
-
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
+$(document).ready(function () {
 	initializePage();
-})
+});
 
 /*
  * Function that is called when the document is ready.
@@ -14,34 +12,32 @@ function initializePage() {
 	$('#next').click(clickedNext);
 }
 
-
-
 function clickedNext(e) {
-    e.preventDefault();
-    const next_question_id = parseInt(getUrlVars()["id"]) + 1;
+	e.preventDefault();
+	const next_question_id = parseInt(getUrlVars()['id']) + 1;
 
-    // TODO: Hard coded number of questions to be 4, should be dynamic
-    if(next_question_id > 3) {
-        location.href="/complete"
-        console.log("complete")
-    }
-    else {
-        location.href = "/quiz?id=" + next_question_id;
-    }
+	// TODO: Hard coded number of questions to be 4, should be dynamic
+	if (next_question_id > 3) {
+		location.href = '/complete';
+		//console.log("complete")
+	} else {
+		location.href = '/quiz?id=' + next_question_id;
+	}
 }
 
 // TODO fix
 // Read a page's GET URL variables and return them as an associative array.
 // https://stackoverflow.com/questions/4656843/get-querystring-from-url-using-jquery
-function getUrlVars()
-{
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
+function getUrlVars() {
+	var vars = [],
+		hash;
+	var hashes = window.location.href
+		.slice(window.location.href.indexOf('?') + 1)
+		.split('&');
+	for (var i = 0; i < hashes.length; i++) {
+		hash = hashes[i].split('=');
+		vars.push(hash[0]);
+		vars[hash[0]] = hash[1];
+	}
+	return vars;
 }
--- routes/correct.js
+++ routes/correct.js
@@ -1,15 +1,15 @@
 // GET ARM quiz page
-const quiz = require('../game.json');
+const quiz = require('/data/game.json');
 
 exports.view = function (req, res) {
 	var questionID = req.query;
-	question_number = parseInt(questionID["id"]);
+	question_number = parseInt(questionID['id']);
 
 	// data to pass to template
-	let data = quiz["questions"][question_number];
-	
+	let data = quiz['questions'][question_number];
+
 	data.layout = 'correct';
 	console.log(data);
-	
+
 	res.render('correct', data);
 };
