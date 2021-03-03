cda527c28fa69ec93c3aab4ef2041b80da2af16a
Hunter Estrada <hestrada@ucsd.edu>
2021-03-03 01:15:41 -0800

    navbar styling | hamburger menu implemented | overlayed buttons on AR | eliminated horizontal/vertical scroll

--- public/js/index.js
+++ public/js/index.js
@@ -46,11 +46,26 @@ const switchFact = (e) => {
 	info.innerHTML = newInfo;
 };
 
+function closeModal() {
+	document.getElementById('modal').style.display = 'none';
+	document
+		.querySelector('#options i')
+		.removeEventListener('click', closeModal, true);
+}
+
+function openModal() {
+	document.getElementById('modal').style.display = 'flex';
+	document
+		.querySelector('#options i')
+		.addEventListener('click', closeModal, true);
+}
+
 function loadListeners() {
 	let ar_buttons = document.querySelectorAll('footer button');
 	ar_buttons.forEach((btn) => {
 		btn.addEventListener('click', switchFact, true);
 	});
+	document.querySelector('nav i').addEventListener('click', openModal, true);
 }
 
 window.addEventListener('load', loadListeners, true);
