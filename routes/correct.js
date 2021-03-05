a624e3115506f84c178c4c4ad4b9cef5ed779ed7
stayingqold <finn@ucsd.edu>
2021-03-04 18:29:10 -0800

    added json push()

--- routes/correct.js
+++ routes/correct.js
@@ -1,15 +1,18 @@
 // GET ARM quiz page
 const quiz = require('../public/data/game.json');
+const score = require('../public/data/score.json');
 
 exports.view = function (req, res) {
 	var questionID = req.query;
 	question_number = parseInt(questionID['id']);
 
 	// data to pass to template
-	let data = quiz['questions'][question_number];
+	let data = {};
+	
+	data.question = quiz['questions'][question_number];
 
 	data.layout = 'correct';
-	console.log(data);
-
+	
+	console.log(score);
 	res.render('correct', data);
 };
--- routes/incorrect.js
+++ routes/incorrect.js
@@ -1,14 +1,35 @@
 // GET ARM quiz page
 const quiz = require('../public/data/game.json');
+const score = require('../public/data/score.json');
 
 exports.view = function (req, res) {
-	var questionID = req.query;
-	question_number = parseInt(questionID['id']);
+	var query = req.query;
+	question_number = parseInt(query['id']);
+
+	var choice = req.query['chose'];
 
 	// data to pass to template
-	let data = quiz['questions'][question_number];
+	let data = {};
+	data.incorrect = [];
+	
+	data.question = quiz['questions'][question_number];
+
+	var obj = new Object();
+	obj.question = data.question.question
+	obj.incorrect_answer = choice;
+	// var correct_answer;
+	for(i = 0; i < data.question.answers.length; i++) {
+		if(data.question.answers[i].is_correct) {
+			obj.correct_answer = data.question.answers[i].answer
+		}
+	}
+	// obj.incorrect = {"question": question, "incorrect-answer": incorrect_answer, "correct-answer": correct_answer}
+
+	data.incorrect.push(obj);
+
+	console.log(data);
 
 	data.layout = 'incorrect';
-	console.log(data);
+	// console.log(data);
 	res.render('incorrect', data);
 };
