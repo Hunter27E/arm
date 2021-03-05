c650d825df31dc0543ab7e601f93967d118a9023
stayingqold <finn@ucsd.edu>
2021-03-04 18:34:43 -0800

    added game tutorial screen

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const incorrect = require('./routes/incorrect');
 const correct = require('./routes/correct');
 const complete = require('./routes/complete');
 const signup = require('./routes/signup');
+const game_intro = require('./routes/game-intro');
 
 // create app and set port
 const port = process.env.PORT || 3000;
@@ -58,6 +59,7 @@ app.get('/correct', correct.view);
 app.get('/complete', complete.view);
 app.get('/signup', signup.view);
 app.get('/login', login.view);
+app.get('/game-intro', game_intro.view);
 
 
 // create server and listen on port
