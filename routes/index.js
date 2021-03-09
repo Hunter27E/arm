d97e9c169a3b39460c4e70c45b21b62ae1837e09
stayingqold <finn@ucsd.edu>
2021-03-09 12:06:14 -0800

    no hamburger menu on pages that shouldn't have one

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 // GET ARM home page
 
-const buttonNames = require('../public/data/buttonsN.json');
+const ardata = require('../public/data/ardata.json');
 
 exports.view = function (req, res) {
+	// get name of model to show user
+	let exhibit = req.query.model;
 	// data to pass to template
 	let data = {};
-	data.btns = buttonNames;
+	data.btns = ardata[exhibit];
+	data.initialFact = data.btns.buttons[0].fact;
+	data.exhibit = exhibit;
 	data.layout = 'index';
 	// console.log(data);
 	res.render('index', data);
